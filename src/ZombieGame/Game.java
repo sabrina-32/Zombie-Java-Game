eaa2df5ca4abf26f962914d849a3b7d234b8cd43
sabrina-32 <sabrina32@student.sust.edu>
2022-07-14 22:04:51 +0600

    start and  stop method

--- src/ZombieGame/Game.java
+++ src/ZombieGame/Game.java
@@ -9,9 +9,31 @@ import java.awt.Canvas;
  */
 public class Game  extends Canvas implements Runnable{
     
+    private  boolean isRunning = false;
+    private Thread thread;
+    
     
     public Game(){
         new Window(1000, 600, "Zombie Game", this );
+        start();
+    }
+    
+//    Game Start method
+    private void start(){
+      isRunning = true;
+      thread = new Thread(this);
+      thread.start();
+    }
+    
+    //    Game Stop method
+    private void stop(){
+        isRunning = false;
+       try{
+          thread.join();
+       } catch (InterruptedException e){
+           e.printStackTrace();
+       }
+       
     }
     public static void main(String[] args) {
         
