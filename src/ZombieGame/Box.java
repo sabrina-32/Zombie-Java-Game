19fb76cf5220a453aa6a08bf483b595b10d1bcff
sabrina-32 <sabrina32@student.sust.edu>
2022-07-15 11:38:12 +0600

    box class

--- src/ZombieGame/Box.java
+++ src/ZombieGame/Box.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ZombieGame;
 
-/**
- *
- * @author Sabrina
- */
-public class Box {
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+
+public class Box extends GameObject {
+    
+    public  Box(int x, int y){
+        super(x,y);
+    }
+
+    @Override
+    public void tick() {
+    }
+
+    @Override
+    public void render(Graphics g) {
+    }
+
+    @Override
+    public Rectangle getBounds() {
+        return null;
+    }
     
 }
