439065cbf71ef3b3f0b28b0ead06dc346c238e95
sabrina-32 <sabrina32@student.sust.edu>
2022-07-16 11:42:13 +0600

    setting movement

--- src/ZombieGame/Protagonist.java
+++ src/ZombieGame/Protagonist.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ZombieGame;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
 /**
  *
  * @author Sabrina
  */
-public class Protagonist {
+public class Protagonist extends GameObject {
+    
+    Handler handler;
+    public Protagonist(int x,  int y, ID id, Handler handler){
+        super(x,y,id);
+        this.handler = handler;
+    }
+
+    @Override
+    public void tick() {
+        x +=velX;
+        y +=velY;
+        
+        
+//      Movement  
+        if(handler.isUp()) velY = -5;
+        else if(!handler.isDown()) velY =0;
+        
+        if(handler.isDown()) velY =5;
+        else if(!handler.isUp()) velY =0;
+        
+        if(handler.isRight()) velX =5;
+        else if(!handler.isLeft()) velX =0;
+        
+        if(handler.isLeft()) velX =5;
+        else if(!handler.isRight()) velX =0;
+        
+        
+        
+    }
+
+    @Override
+    public void render(Graphics g) {
+    }
+
+    @Override
+    public Rectangle getBounds() {
+        return null;
+    }
     
 }
