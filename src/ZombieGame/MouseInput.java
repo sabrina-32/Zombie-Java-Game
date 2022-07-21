2774e3a08c63a2748161832d651bc1aa57f684af
sabrina-32 <sabrina32@student.sust.edu>
2022-07-21 10:12:17 +0600

    mouse input

--- src/ZombieGame/MouseInput.java
+++ src/ZombieGame/MouseInput.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ZombieGame;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  *
  * @author Sabrina
  */
-public class MouseInput {
+public class MouseInput  extends MouseAdapter{
+   
+    private Handler  handler;
+    private Camera camera;
     
+    public MouseInput(Handler  handler,Camera camera){
+        this.handler = handler;
+        this.camera = camera;
+    }
+    public void mousePresses(MouseEvent e){
+       
+        int mx =(int) (e.getX() + camera.getX());
+        int my =(int) (e.getY() + camera.getY());
+
+    }
 }
