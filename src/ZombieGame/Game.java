74340747b3840fb7a48727c17f2e1bf980f2e8de
sabrina-32 <sabrina32@student.sust.edu>
2022-07-21 10:37:00 +0600

    enemy added

--- src/ZombieGame/Game.java
+++ src/ZombieGame/Game.java
@@ -142,6 +142,10 @@ public class Game  extends Canvas implements Runnable{
                 if(blue == 255)
                     handler.addObject(new Protagonist(xx*32,  yy*32,  ID.Player, handler));
                 
+                if(green ==255){
+                                    handler.addObject(new Enemy(xx*32,  yy*32,  ID.Enemy, handler));
+    
+                }
 
             }
         }
