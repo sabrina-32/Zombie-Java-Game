419835809ffad900688c7f59ddc59360092c717a
sabrina-32 <sabrina32@student.sust.edu>
2022-07-21 10:00:00 +0600

    setting camera for  following protagonist

--- src/ZombieGame/Camera.java
+++ src/ZombieGame/Camera.java
@@ -10,10 +10,7 @@ public class Camera {
     
      private  float  x,y;
     
-    public Camera(float x,  float y){
-        this.x = x;
-        this.y= y;
-    }
+   
     
     public  void  tick(GameObject object){
         
